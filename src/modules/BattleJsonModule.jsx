25898b83548942492ace9ccba40241244aa44e12
gimun <gimun1990@gmail.com>
2024-09-02 17:33:01 +0900

    battle history 구조 작업 진행 중 (mock-data 수정)

--- src/modules/BattleJsonModule.jsx
+++ src/modules/BattleJsonModule.jsx
@@ -1,31 +1,31 @@
-import {useCallback, useState, useEffect} from 'react';
+import {useCallback, useState, useEffect, useMemo} from 'react';
 import DataDisplay from '../components/DataDisplay.jsx';
 import {COLUMNS, LABELS, SORT} from '../constants/Keys.js';
-import historyData from '../mock-data/battle/clan_war_20240819.json';
+import historyData from '../mock-data/battle/clan_war_20240826.json';
 import {getMemberName} from '../utils/memberHelper.js';
 
 const BattleJsonModule = () => {
-    const [data, setData] = useState(historyData);
+    const [data, setData] = useState([]);
     const [config, setConfig] = useState({
         sort: {key: COLUMNS.SCORE, direction: SORT.DESC},
         search: {term: '', placeholder: LABELS[COLUMNS.NAME] || 'Name'}
     });
 
-    // UseEffect to set the name value based on member_id when the component mounts
     useEffect(() => {
-        const updatedData = data.map(item => ({
+        // Map over the initial data to add names
+        const updatedData = historyData.map(item => ({
             ...item,
-            [COLUMNS.NAME]: getMemberName(item[COLUMNS.MEMBER_ID]) // Set the name using getMemberName
+            [COLUMNS.NAME]: getMemberName(item[COLUMNS.MEMBER_ID])
         }));
         setData(updatedData);
-    }, []);  // Add an empty dependency array to run the effect only once on mount
+    }, []);
 
-    const columns = [
+    const columns = useMemo(() => [
         {key: COLUMNS.MEMBER_ID, label: "", flex: 1, align: 'center', type: 'number', isKey: true},
         {key: COLUMNS.RANK, label: LABELS[COLUMNS.RANK], flex: 1, align: 'center', type: 'number'},
         {key: COLUMNS.NAME, label: LABELS[COLUMNS.NAME], flex: 4, align: 'center', type: 'string'},
         {key: COLUMNS.SCORE, label: LABELS[COLUMNS.SCORE], flex: 4, align: 'center', type: 'number'}
-    ];
+    ], []);
 
     const handleSort = useCallback((key) => {
         setConfig(prevConfig => {
@@ -44,25 +44,25 @@ const BattleJsonModule = () => {
         }));
     }, []);
 
-    const sortedDataList = [...data].sort((a, b) => {
-        if (config.sort.key) {
+    const filteredAndSortedData = useMemo(() => {
+        const filteredData = data.filter(item =>
+            item[COLUMNS.NAME]?.toString().toLowerCase().includes(config.search.term.toLowerCase())
+        );
+
+        return filteredData.sort((a, b) => {
             const aValue = a[config.sort.key];
             const bValue = b[config.sort.key];
 
             if (aValue < bValue) return config.sort.direction === SORT.ASC ? -1 : 1;
             if (aValue > bValue) return config.sort.direction === SORT.ASC ? 1 : -1;
-        }
-        return 0;
-    });
-
-    const filteredData = sortedDataList.filter(item =>
-        item[COLUMNS.NAME]?.toString().toLowerCase().includes(config.search.term.toLowerCase())
-    );
+            return 0;
+        });
+    }, [data, config]);
 
     return (
         <>
             <DataDisplay
-                data={filteredData}
+                data={filteredAndSortedData}
                 columns={columns}
                 onSort={handleSort}
                 config={config}
