e2c5b12bf741e592956dd15f66e91a5a4062a205
gimun <gimun1990@gmail.com>
2024-09-07 13:07:07 +0900

    PhotoSlider 개선 및 냥이 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // src/App.jsx
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import AppLayout from './layout/AppLayout';
+import AppLayout2 from './layout/AppLayout2';
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<AppLayout/>}></Route>
+                <Route path="/test" element={<AppLayout2/>}></Route>
             </Routes>
         </BrowserRouter>
     );
