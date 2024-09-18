b707c96f63938ba0a57f53258b8da0fda94a6b31
gimun <gimun1990@gmail.com>
2024-09-18 16:24:14 +0900

    미사용 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 // src/App.jsx
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import AppLayout from './components/templates/AppLayout.jsx';
-import AppLayout2 from './components/templates/AppLayout2.jsx';
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<AppLayout/>}></Route>
-                <Route path="/test" element={<AppLayout2/>}></Route>
             </Routes>
         </BrowserRouter>
     );
