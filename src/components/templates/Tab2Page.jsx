eb68dffd327c11708628e2f09e552359df83e2b7
gimunhan <gimun.han@mytrum.com>
2024-12-30 12:30:03 +0900

    20241229 클랜전 반영

--- src/components/templates/Tab2Page.jsx
+++ src/components/templates/Tab2Page.jsx
@@ -23,7 +23,7 @@ const Tab2Page = () => {
     {
       key: 'subTab1',
       label: '월간 랭킹 (new)',
-      component: <BattleRankNewModule isMonthly={false} gameCount={12} />,
+      component: <BattleRankNewModule isMonthly={false} gameCount={15} />,
     },
     {
       key: 'subTab11',
