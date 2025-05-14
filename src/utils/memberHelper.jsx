f9f63010859e5fcaabdb6c089f24111d1f11c741
gimunhan <gimun.han@mytrum.com>
2025-05-14 09:56:11 +0900

    멤버 데이터 업데이트

--- src/utils/memberHelper.jsx
+++ src/utils/memberHelper.jsx
@@ -1,7 +1,7 @@
 // Enum-like object with numeric status
 export const members = {
   1: { name: '무니는 야옹', status: 1 },
-  2: { name: '주키니콩', status: 1 },
+  2: { name: '주키니콩', status: 0 },
   3: { name: '유디는 야옹', status: 1 },
   4: { name: '왕떡이는 야옹', status: 1 },
   5: { name: '졔타몽', status: 1 },
