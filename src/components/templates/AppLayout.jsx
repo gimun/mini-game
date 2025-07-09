3079be402338aeff228a8f7dfbb5090b9c0ad3f5
gimun <gimun1990@gmail.com>
2025-07-10 00:24:30 +0900

    7월 기록 반영

--- src/components/templates/AppLayout.jsx
+++ src/components/templates/AppLayout.jsx
@@ -59,7 +59,7 @@ const AppLayoutComponent = () => {
       label: '냥냥 패밀리',
       component: <MemberJsonModule />,
       protected: false,
-      footerText: '데이터 최신화: 2025-06-23',
+      footerText: '데이터 최신화: 2025-07-10',
       showFooter: true,
     },
     {
